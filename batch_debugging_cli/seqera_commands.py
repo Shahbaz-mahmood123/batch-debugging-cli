d1a06d001a8b53ecc0a5b47c84fb96a7ea57180d
Shahbaz Mahmood <shahbazmahmood@Shahbazs-MacBook-Pro.local>
2023-12-13 17:22:24 -0500

    adding else

--- batch_debugging_cli/seqera_commands.py
+++ batch_debugging_cli/seqera_commands.py
@@ -15,13 +15,15 @@ class SeqeraCommands(SeqeraCommandsInterface):
     def __init__(self, seqera_compute_env_wrapper = None) -> None:
         self.seqera_compute_env_wrapper = SeqeraComputeEnvsWrapper()
     
+    def debug_compute_enviornment(self, compute_env_id: str):
+        pass
+    
     def optimize_compute_enviornment(self, compute_env_id: str):
         
         if compute_env_id:
             compute_env_response = self.seqera_compute_env_wrapper.get_compute_env(compute_env_id)
             recommendations = self.seqera_compute_env_wrapper.optimize_compute_env(compute_env_response)
             print(recommendations)
-
-
-# test = seqera.get_compute_env("5tQSF2ahyA19GNS5b8rzNS")
-# recommendations = seqera.optimize_compute_env(test)
+        else:
+            print("Please provide a valid compute enviornment ID")
+            
